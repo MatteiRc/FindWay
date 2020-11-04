8363f6162c5be96d2c0618a5bf1a9325560e394a
Shockwaiver <38588489+Shockwaiver@users.noreply.github.com>
2020-11-04 00:28:28 -0300

    Commit Adm funcional

--- src/components/AdmPainel.js
+++ src/components/AdmPainel.js
@@ -25,11 +25,14 @@ import axios from 'axios';
 export default class ListaProduto extends Component {
 
   state = {
-    filtros:"",
-    search: "",
-    servicos:[],
 	usuarios: [],
-	displayUsuarios: false
+	empresas: [],
+	curriculos: [],
+	vagas: [],
+	displayUsuarios: false,
+	displayEmpresas: false,
+	displayCurriculos: false,
+	displayVagas:false
   };
 
  componentDidMount(){
@@ -41,16 +44,73 @@ export default class ListaProduto extends Component {
 		for(let i = 0; i < data.length; i++){
 		  let usuario = {
 			id: data[i].id,
-			nome: data[i].nome
+			nome: data[i].nome,
+			telefone: data[i].telefone,
+			estado: data[i].estado,
+			cidade: data[i].cidade,
+			bairro: data[i].bairro,
+			cep: data[i].cep,
+			email: data[i].email
 		  }
 		  arr.push(usuario);
 		}
 		this.setState({usuarios:arr});
 	  })
+  axios.get("http://localhost:3001/admEmpresas")
+	  .then(res =>{
+		let arr = new Array();
+		let data = JSON.parse(JSON.stringify(res.data));
+		//console.log(data);
+		for(let i = 0; i < data.length; i++){
+		  let empresa = {
+			id: data[i].id,
+			nome: data[i].nome,
+			telefone: data[i].telefone,
+			estado: data[i].estado,
+			cidade: data[i].cidade,
+			bairro: data[i].bairro,
+			cep: data[i].cep,
+			email: data[i].email,
+			cnpj: data[i].cnpj,
+			cargo: data[i].cargo
+		  }
+		  arr.push(empresa);
+		}
+		this.setState({empresas:arr});
+	  })
+  axios.get("http://localhost:3001/admCurriculos")
+	  .then(res =>{
+		let arr = new Array();
+		let data = JSON.parse(JSON.stringify(res.data));
+		//console.log(data);
+		for(let i = 0; i < data.length; i++){
+		  let curriculo = {
+			id: data[i].id,
+			id_usuario: data[i].id_usuario,
+			descricao: data[i].descricao
+		  }
+		  arr.push(curriculo);
+		}
+		this.setState({curriculos:arr});
+	  })
+  axios.get("http://localhost:3001/admVagas")
+	  .then(res =>{
+		let arr = new Array();
+		let data = JSON.parse(JSON.stringify(res.data));
+		//console.log(data);
+		for(let i = 0; i < data.length; i++){
+		  let vaga = {
+			id: data[i].id,
+			id_usuario: data[i].id_usuario,
+			descricao: data[i].descricao
+		  }
+		  arr.push(vaga);
+		}
+		this.setState({vagas:arr});
+	  })
  }
 
  renderUsuario = usuario =>{
-		console.log(usuario);
 	return (
 	<div>
 		<p>{usuario.id}</p>
@@ -58,9 +118,90 @@ export default class ListaProduto extends Component {
 	)
  };
  
- displayUsuario = () => {
+ renderUsuario = usuario =>{
+	return (
+	 <tr>
+		<td>{usuario.id}</td>
+		<td>{usuario.nome}</td>
+		<td>{usuario.telefone}</td>
+		<td>{usuario.estado}</td>
+		<td>{usuario.cidade}</td>
+		<td>{usuario.bairro}</td>
+		<td>{usuario.cep}</td>
+		<td>{usuario.email}</td>
+	</tr>
+	)
+ };
+  renderCurriculo = curriculo =>{
+	return (
+	 <tr>
+		<td>{curriculo.id}</td>
+		<td>{curriculo.id_usuario}</td>
+		<td>{curriculo.descricao}</td>
+	</tr>
+	)
+ };
+ 
+ renderEmpresa = empresa =>{
+	return (
+	 <tr>
+		<td>{empresa.id}</td>
+		<td>{empresa.nome}</td>
+		<td>{empresa.telefone}</td>
+		<td>{empresa.estado}</td>
+		<td>{empresa.cidade}</td>
+		<td>{empresa.bairro}</td>
+		<td>{empresa.cep}</td>
+		<td>{empresa.email}</td>
+		<td>{empresa.cnpj}</td>
+		<td>{empresa.cargo}</td>
+	</tr>
+	)
+ };
+ 
+ renderVaga = vaga =>{
+	return (
+	 <tr>
+		<td>{vaga.id}</td>
+		<td>{vaga.id_usuario}</td>
+		<td>{vaga.descricao}</td>
+	</tr>
+	)
+ };
+ 
+ displayUsuarios = () => {
     this.setState({
-        displayUsuarios: !this.state.displayUsuarios
+        displayUsuarios: !this.state.displayUsuarios,
+		displayEmpresas: false,
+		displayCurriculos: false,
+		displayVagas: false
+    })
+}
+
+ displayEmpresas = () => {
+    this.setState({
+		displayEmpresas: !this.state.displayEmpresas,
+		displayUsuarios: false,
+		displayCurriculos: false,
+		displayVagas: false
+    })
+}
+
+ displayCurriculos = () => {
+    this.setState({
+		displayCurriculos: !this.state.displayCurriculos,
+		displayEmpresas: false,
+		displayUsuarios: false,
+		displayVagas: false
+    })
+}
+
+ displayVagas = () => {
+    this.setState({
+        displayVagas: !this.state.displayVagas,
+		displayEmpresas: false,
+		displayCurriculos: false,
+		displayUsuarios: false
     })
 }
 
@@ -71,8 +212,61 @@ export default class ListaProduto extends Component {
 	if ( this.state.displayUsuarios ) {
      var usuarios = (
      <div>
-		{filteredServicos.usuarios.map(usuario => {
-		return this.renderUsuario(usuario);
+	    <th>ID</th>
+		<th>Nome</th>
+		<th>Telefone</th>
+		<th>Estado</th>
+		<th>Cidade</th>
+		<th>Bairro</th>
+		<th>CEP</th>
+		<th>Email</th>
+			{filteredServicos.usuarios.map(usuario => {
+			return this.renderUsuario(usuario);
+			})}
+     </div>
+     )
+	}
+	
+	if ( this.state.displayCurriculos ) {
+     var curriculos = (
+     <div>
+	 		<th>ID</th>
+			<th>ID_Usuario</th>
+			<th>Descrição</th>
+		{filteredServicos.curriculos.map(curriculo => {
+		return this.renderCurriculo(curriculo);
+		})}
+     </div>
+     )
+	}
+	
+	if ( this.state.displayEmpresas ) {
+     var empresas = (
+     <div>
+			<th>ID</th>
+			<th>Nome</th>
+			<th>Telefone</th>
+			<th>Estado</th>
+			<th>Cidade</th>
+			<th>Bairro</th>
+			<th>CEP</th>
+			<th>Email</th>
+			<th>CNPJ</th>
+			<th>Cargo</th>
+		{filteredServicos.empresas.map(empresa => {
+		return this.renderEmpresa(empresa);
+		})}
+     </div>
+     )
+	}
+	if ( this.state.displayVagas ) {
+     var vagas = (
+     <div>
+			<th>ID</th>
+			<th>ID_Usuario</th>
+			<th>Descrição</th>
+		{filteredServicos.vagas.map(vaga => {
+		return this.renderVaga(vaga);
 		})}
      </div>
      )
@@ -81,13 +275,15 @@ export default class ListaProduto extends Component {
     return (
       <div>
             <Titulo nome="Painel de administrador"></Titulo>
-			      <Button onClick={this.displayUsuario} 
+			      <Button onClick={this.displayUsuarios} 
 				  variant="contained">Usuarios</Button>
 
-			<Button  variant="contained">Curriculos</Button>
-			<Button  variant="contained">Empresas</Button>
-			<Button  variant="contained">Vagas</Button>
-					{usuarios}
+			<Button onClick={this.displayCurriculos}  variant="contained">Curriculos</Button>
+			<Button onClick={this.displayEmpresas}  variant="contained">Empresas</Button>
+			<Button onClick={this.displayVagas}  variant="contained">Vagas</Button>
+			<table>
+			{usuarios} {curriculos} {empresas} {vagas}
+			</table>
       </div>
     );
 
